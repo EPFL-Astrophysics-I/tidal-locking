98cdf2491fe1c5b31e8a048f54157153ea487d53
Théo Damiani <theo.damiani@epfl.ch>
2022-10-13 01:02:59 +0200

    2body pb + sprites + shaders

---
+++ Assets/OrbitSimulation/Scripts/OrbitSlideController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitSlideController : SimulationSlideController
+{
+
+    // Start is called before the first frame update
+    public override void InitializeSlide()
+    {
+        OrbitSimulation sim = simulation as OrbitSimulation;
+    }
+}
---
+++ Assets/SlideManager/TextScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TextScript : MonoBehaviour
+{
+    [SerializeField]
+    LanguageToggle languageToggle;
+    
+    [SerializeField] 
+    private TextMeshProUGUI tmpUI;
+
+    [SerializeField] 
+    private string FR;
+
+    private string oldFR = "";
+
+    [SerializeField] 
+    private string EN;
+
+    private string oldEN = "";
+
+    void OnValidate() {
+        if (FR != oldFR) {
+            tmpUI.text = FR;
+            oldFR = FR;
+        }
+        if (EN != oldEN) {
+            tmpUI.text = EN;
+            oldEN = EN;
+        }
+    }
+    
+    void Start()
+    {
+        if (languageToggle != null) {
+            languageToggle.OnLanguageToggle += SetLanguage;
+
+            // Set English as the default language.
+            SetLanguage(LanguageToggle.ActiveLanguage.EN);
+        }
+    }
+
+    private void SetLanguage(LanguageToggle.ActiveLanguage language){
+        switch (language) {
+            case LanguageToggle.ActiveLanguage.EN: {
+                tmpUI.text = EN;
+                break;
+            }
+            case LanguageToggle.ActiveLanguage.FR: {
+                tmpUI.text = FR;
+                break;
+            }
+        }
+    }
+}
